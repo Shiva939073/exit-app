5b514792006d7c9e8ac8ccf7ec8074c09609fa3c
Shiva939073 <avvarishivakrishna@gmail.com>
2025-07-08 13:39:39 +0530

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  String userName = "";
+  int xp = 0;
+  int level = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    final uid = _auth.currentUser?.uid;
+    if (uid != null) {
+      final userDoc = await _firestore.collection('users').doc(uid).get();
+      setState(() {
+        userName = userDoc['name'] ?? 'User';
+        xp = userDoc['xp'] ?? 0;
+        level = userDoc['level'] ?? 1;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome back, \$userName", style: TextStyle(color: Colors.white, fontSize: 24)),
+            SizedBox(height: 10),
+            Text("XP: \$xp", style: TextStyle(color: Colors.greenAccent, fontSize: 18)),
+            Text("Level: \$level", style: TextStyle(color: Colors.blueAccent, fontSize: 18)),
+          ],
+        ),
+      ),
+    );
+  }
+}
