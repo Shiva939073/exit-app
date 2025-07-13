420117d96f12d83c9dabcf69e7f3393470b33092
Shiva939073 <avvarishivakrishna@gmail.com>
2025-07-13 11:31:19 +0530

    Create ai_video_interviewer_page.dart

---
+++ lib/screens/ai_video_interviewer_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class AIVideoInterviewerPage extends StatefulWidget {
+  const AIVideoInterviewerPage({super.key});
+
+  @override
+  State<AIVideoInterviewerPage> createState() => _AIVideoInterviewerPageState();
+}
+
+class _AIVideoInterviewerPageState extends State<AIVideoInterviewerPage> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      'https://your-cloud-link.com/ai_interviewer_1.mp4',
+    )..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: const Text('AI Interviewer', style: TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : const CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepPurple,
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
